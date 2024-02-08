bf232d24c9b9249642f598f90e6a999b95cada9e
Cynosure24 <139391583+Cynosure24@users.noreply.github.com>
2024-02-08 17:00:28 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import css from "./App.module.css";
+import css from "./App.css";
 import { useEffect, useState } from "react";
 import { Discription } from "./Discription/Discription";
 import { Feedback } from "./Feedback/Feedback";
